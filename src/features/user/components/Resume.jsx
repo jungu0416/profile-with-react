1cd05455f095714c81955b4708d20e87c6bf4fe5
jungu0416 <jungu0416@naver.com>
2025-04-05 15:18:45 +0900

    chore: supabase, rtk query add

--- src/features/user/components/Resume.jsx
+++ src/features/user/components/Resume.jsx
@@ -4,7 +4,7 @@ function Resume() {
       <h2 className="text-6xl mb-20">Resume</h2>
       <div className="flex w-full mb-60">
         <div className="w-1/2 px-8">
-          <p className="text-2xl text-center">Development</p>
+          <p className="text-2xl text-center font-bold">Development</p>
           <ul>
             <li>
               <Card />
@@ -18,7 +18,7 @@ function Resume() {
           </ul>
         </div>
         <div className="w-1/2 px-8">
-          <p className="text-2xl text-center">Management</p>
+          <p className="text-2xl text-center font-bold">Management</p>
           <ul>
             <li>
               <Card />
@@ -38,13 +38,21 @@ function Resume() {
 
 function Card() {
   return (
-    <div className="m-8 p-4 border-2 rounded-lg border-solid">
-      <div>
-        <span>June 1, 2009 - 2010</span>
+    <div className="m-8 p-4 border rounded-lg border-solid shadow-lg">
+      <div className="mt-[-28px] mb-[10px] ">
+        <span className="p-2 font-bold text-gray-400 rounded-lg bg-white">2024.02 ~ 2024.06</span>
       </div>
       <div>
-        <p>어쩌구 프로젝트</p>
-        어쩌구 내용 어쩌구 내용 어쩌구 내용 어쩌구 내용 어쩌구 내용 어쩌구 내용
+        <p className="mb-2 text-lg]">
+          <span className="text-[#44af2b]">녹십자</span> - 채용 시스템 개발/유지보수 -
+        </p>
+        <span className="text-base text-gray-400">
+          <ul>
+            <li>Batch & Scheduler를 통한 recruit data pipeline 개발</li>
+            <li>SAP Successfactors Auth 연동</li>
+            <li>SAP PI/PO 미들웨어 API 연동</li>
+          </ul>
+        </span>
       </div>
     </div>
   )
