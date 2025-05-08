ab4f13672797d3c2afe3ad15714eca34f1837eee
jungu0416 <jungu0416@naver.com>
2025-05-08 14:31:57 +0900

    fix: resume hide

--- src/features/user/User.jsx
+++ src/features/user/User.jsx
@@ -6,12 +6,12 @@ import Enterprise from './components/Enterprise'
 function User() {
   return (
     <main>
-      <section className="flex h-[4300px] relative justify-center">
+      <section className="flex h-[3300px] relative justify-center">
         <article className="w-[98%] mt-[-30px] pb-8 bg-white absolute rounded-xl shadow-lg backdrop-blur-lg bg-opacity-80">
           <Info />
           <div className="flex flex-col justify-center items-center w-container mx-auto mt-40">
             <About />
-            <Resume />
+            {/* <Resume /> */}
             <Enterprise />
           </div>
         </article>
